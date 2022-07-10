a6f2ac347ab6de712cac956e64a1b7a336da7b34
dpsmnsk <dpsmnsk@gmail.com>
2022-07-10 20:46:40 +0500

    add pagination and indexes

---
+++ ormconfig.mjs
@@ -0,0 +1,15 @@
+import { DataSource }from 'typeorm';
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+});
