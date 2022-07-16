ef27f0e161d5140c7846f644b4cce5ad350ac8b6
dpsmnsk <dpsmnsk@gmail.com>
2022-07-16 12:59:22 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
@@ -16,7 +17,7 @@ async function bootstrap() {
       },
     }),
   );
-  app.useGlobalInterceptors(new WrapResponseInterceptor())
+  app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor())
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
