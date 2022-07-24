7e58950722a25946285fcf7e96ea51396e0878f2
Dima Pasumansky <dpsmnsk@gmail.com>
2022-07-24 22:53:34 +0500

    upd

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,24 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const CreateMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+});
+
 describe('CoffeesService', () => {
   let service: CoffeesService;
+  let coffeeRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CoffeesService,
         { provide: Connection, useValue: {} },
-        { provide: getRepositoryToken(Flavor), useValue: {} },
-        { provide: getRepositoryToken(Coffee), useValue: {} },
+        {
+          provide: getRepositoryToken(Flavor),
+          useValue: CreateMockRepository(),
+        },
+        {
+          provide: getRepositoryToken(Coffee),
+          useValue: CreateMockRepository(),
+        },
       ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
+    coffeeRepository = module.get<MockRepository>(getRepositoryToken(Coffee));
   });
 
   it('should be defined', () => {
@@ -31,6 +45,7 @@ describe('CoffeesService', () => {
         const coffeeId = '1';
         const expectedCoffee = {};
 
+        coffeeRepository.findOne.mockReturnValue(expectedCoffee);
         const coffee = await service.findOne(coffeeId);
         expect(coffee).toEqual(expectedCoffee);
       });
